650bdb05170dd3d51cc6161eacb86cf06d9fb768
Andressa Atelli <andressaatelli.programador@gmail.com>
2022-05-23 13:18:01 -0300

    Add files via upload

---
+++ Calculator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Console.WriteLine("Digite o primeiro valor: ");
+            float valor1 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o segundo valor: ");
+            float valor2 = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            float resultado = valor1 + valor2;
+            Console.WriteLine($"O resultado da soma é {resultado}");
+        }
+    }
+} 
